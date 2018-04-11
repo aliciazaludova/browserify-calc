0c7f7517e141fd7146ea2cee6a625613534b1ff9
Alicia Zaludova <aliciazaludova@gmail.com>
2018-04-10 20:15:42 -0500

    made-multiple-js-files

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
-console.log('peachpuff');
+const add = require('./add');
+const subtract = require('./subtract');
+const multiply = require('./multiply');
+const divide = require('./divide');
+
+console.log('Adding: ', add(1, 2));
+console.log('Subtracting: ', subtract(40, 2));
+console.log('Multiplying: ', multiply(1, 2));
+console.log('Dividing: ', divide(10, 5));
